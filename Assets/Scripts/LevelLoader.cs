14943443b054d57912ee4b2ff79fe8f3a04158c4
NateFrogHI <n.k.mitson@gmail.com>
2021-10-16 22:57:27 -0700

    with splash screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 4;
+    int currentSceneIndex;
+
+    private enum Scene
+    {
+        Splash,
+        Start
+    }
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == (int)Scene.Splash)
+        {
+            StartCoroutine(WaitAndLoad());
+        }
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        LoadStartScene();
+    }
+
+    public void LoadStartScene()
+    {
+        currentSceneIndex = (int)Scene.Start;
+        SceneManager.LoadScene((int)Scene.Start);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(++currentSceneIndex);
+    }
+}
